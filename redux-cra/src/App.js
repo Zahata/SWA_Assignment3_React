ff40627e1a8ce0d90e65d96fc092f37c4adedcfe
Zahata <253925@via.dk>
2019-11-08 14:58:57 +0100

    Initial

---
+++ redux-cra/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction';
+
+import logo from './logo.svg';
+import './App.css';
+
+const mapStateToProps = state => ({
+  ...state
+ })
+
+     const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ })
+
+class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+   }
+ render() {
+  return (
+   <div className="App">
+    <header className="App-header">
+     <img src={logo} className="App-logo" alt="logo" />
+     <h1 className="App-title">Welcome to React</h1>
+    </header>  
+    <button onClick={this.simpleAction}>Test redux action</button>
+    <pre>
+      {
+        JSON.stringify(this.props)
+      }
+    </pre>
+    <p className="App-intro">
+     To get started, edit <code>src/App.js</code> and save to reload
+    </p>
+   </div>
+  );
+ }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
